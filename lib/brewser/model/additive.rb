1a5329fe3da185365ea4568d6bdb48d02f5f8a12
Jon Lochner <jlochner@gmail.com>
2012-05-18 15:50:56 -0400

    Renamed attribute

--- lib/brewser/model/additive.rb
+++ lib/brewser/model/additive.rb
@@ -1,12 +1,12 @@
 module Brewser
   class Additive < Model
     property :name, String, :required => true
+    property :description, String, :length => 65535
     property :type, String, :set => ['Spice', 'Fining', 'Water Agent', 'Herb', 'Flavor', 'Other'], :required => true
     property :added_when, String, :set => ['Boil', 'Mash', 'Primary', 'Secondary', 'Bottling'], :required => true
     property :time, Time, :required => true
     property :amount, String, :required => true
 
     property :use_for, String
-    property :notes, String, :length => 65535
   end
 end
