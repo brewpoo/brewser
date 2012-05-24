2d76435d059a3680d087bfe468fb7f570066984b
Jon Lochner <jlochner@gmail.com>
2012-05-23 21:16:59 -0400

    Started working on ProMash rec

--- lib/brewser/engines/promash_rec.rb
+++ lib/brewser/engines/promash_rec.rb
@@ -11,6 +11,69 @@ class ProMashRec < Brewser::Engine
     
   end
   
+  module Loader
+    
+    require 'bindata'
+    
+    class Hop < BinData::Record
+      endian :little
+      string :test
+    end
+  end
+  
 end
 
 
+class ProMashRec::Hop < Brewser::Hop
+  
+  def from_promash(data)
+    return Loader::Hop.new(data)
+  end
+  
+end
+
+class ProMashRec::Fermentable < Brewser::Fermentable
+
+  
+end
+
+class ProMashRec::Additive < Brewser::Additive
+
+  
+end
+
+class ProMashRec::Yeast < Brewser::Yeast
+  
+  
+end
+
+class ProMashRec::MashStep < Brewser::MashStep
+  
+end
+
+class ProMashRec::MashSchedule < Brewser::MashSchedule
+  
+end
+
+class ProMashRec::FermentationStep < Brewser::FermentationStep
+    
+end
+
+class ProMashRec::FermentationSchedule < Brewser::FermentationSchedule 
+  
+end
+
+class ProMashRec::WaterProfile < Brewser::WaterProfile
+
+
+end
+
+class ProMashRec::Style < Brewser::Style
+
+  
+end
+
+class ProMashRec::Recipe < Brewser::Recipe
+
+
+end
