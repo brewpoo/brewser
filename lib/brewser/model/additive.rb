789c116f09c0c549e3de460eb678df5486c846af
Jon Lochner <jlochner@gmail.com>
2012-05-26 21:07:04 -0400

    Added Fruit

--- lib/brewser/model/additive.rb
+++ lib/brewser/model/additive.rb
@@ -4,7 +4,7 @@ module Brewser
     
     property :name, String, :required => true
     property :description, String, :length => 65535
-    property :type, String, :set => ['Spice', 'Fining', 'Water Agent', 'Herb', 'Flavor', 'Other'], :required => true
+    property :type, String, :set => ['Spice', 'Fining', 'Water Agent', 'Herb', 'Flavor', 'Fruit', 'Other'], :required => true
     property :added_when, String, :set => ['Boil', 'Mash', 'Primary', 'Secondary', 'Packaging'], :required => true
     property :time, Time, :required => true
     property :amount, WeightOrVolume, :required => true
