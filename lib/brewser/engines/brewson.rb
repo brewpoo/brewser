ef91e0e85afd5df9d29d02889b2e0f9cf49b796b
Jon Lochner <jlochner@gmail.com>
2012-06-26 14:04:34 -0400

    Updated to use JSON

--- lib/brewser/engines/brewson.rb
+++ lib/brewser/engines/brewson.rb
@@ -7,10 +7,11 @@ class BrewSON < Brewser::Engine
     end
   
     def deserialize(string_or_io)
+      JSON.parse(string_or_io)
     end
     
     def serialize(brewser_model)
-      brewser_model.deep_json
+      JSON.generate(brewser_model)
     end
     
   end
